3e8d47f5d37c6b74d03a65b5aef18de3f926c624
walkor <worker-man@qq.com>
2014-12-30 22:44:55 +0800

    fix

--- bootstrap.php
+++ bootstrap.php
@@ -9,7 +9,7 @@ use WorkerMan\Worker;
 
 require_once __DIR__ . '/Workerman/Worker.php';
 
-foreach(glob(__DIR__.'/../applications/*/Bootstrap/*.php') as $start_file)
+foreach(glob(__DIR__.'/applications/*/Bootstrap/*.php') as $start_file)
 {
     require_once $start_file;;
 }
