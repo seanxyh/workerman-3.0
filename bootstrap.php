21f7688645bb144bad8f05bca2b359d527257283
walkor <worker-man@qq.com>
2014-12-30 22:37:26 +0800

    bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -7,7 +7,7 @@
 ini_set('display_errors', 'on');
 use WorkerMan\Worker;
 
-require_once __DIR__ . 'Workerman/Worker.php';
+require_once __DIR__ . '/Workerman/Worker.php';
 
 foreach(glob(__DIR__.'/../applications/*/Bootstrap/*.php') as $start_file)
 {
