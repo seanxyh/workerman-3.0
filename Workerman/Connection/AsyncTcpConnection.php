48f6d08d525c587aa7dc40ecd40b996febaec8b4
walkor <worker-man@qq.com>
2015-01-25 21:59:18 +0800

    AysncTcpConnection protocol

--- Workerman/Connection/AsyncTcpConnection.php
+++ Workerman/Connection/AsyncTcpConnection.php
@@ -36,13 +36,13 @@ class AsyncTcpConnection extends TcpConnection
         if($scheme != 'tcp')
         {
             $scheme = ucfirst($scheme);
-            $this->protocol = '\\Protocols\\'.$scheme;;
+            $this->protocol = '\\Protocols\\'.$scheme;
             if(!class_exists($this->protocol))
             {
-                $this->protocol = '\\Protocols\\'.$scheme . '\\' . $scheme;
+                $this->protocol = '\\Workerman\\Protocols\\' . $scheme;
                 if(!class_exists($this->protocol))
                 {
-                    throw new Exception('class ' .$this->protocol . ' not exist');
+                    throw new Exception("class \\Protocols\\$scheme not exist");
                 }
             }
         }
